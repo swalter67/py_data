f8398282d190d2b1f1bb5a446be94097e382e293
swalter <swalter67@gmail.com>
2023-11-17 17:27:18 +0100

    58

---
+++ day-4/ex02/callLimit.py
@@ -0,0 +1,19 @@
+from typing import Any
+
+def callLimit(limit: int):
+    count = 0
+
+    def callLimiter(function):
+        def limit_function(*args: Any, **kwds: Any):
+            nonlocal count
+            try:
+                count += 1
+                if count <= limit:
+                    return function(*args, **kwds)
+                else:
+                    print(f"Error: {function} call too many times")
+            except AssertionError as e:
+                print("Error:", e )
+        return limit_function
+
+    return callLimiter
---
+++ day-4/ex03/new_student.py
@@ -0,0 +1,19 @@
+import random
+import string
+from dataclasses import dataclass, field
+def generate_id() -> str:
+    """ generate ramdom id 15 char long"""
+    return "".join(random.choices(string.ascii_lowercase, k = 15))
+
+
+
+@dataclass
+class Student:
+    name: str = field(init=True)
+    surname: str = field(init=True)
+    active: bool = field(default=True)
+    login: str = field(init=False)
+    id: str = field(init=False, default=generate_id())
+
+    def __post_init__(self):
+        self.login = self.name[0].capitalize() + self.surname.lower()

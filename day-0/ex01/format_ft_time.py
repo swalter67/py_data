aec469296eeda17b28f6739122f198608345bc7c
swalter <swalter67@gmail.com>
2023-11-28 17:07:03 +0100

    dfefef

--- day-0/ex01/format_ft_time.py
+++ day-0/ex01/format_ft_time.py
@@ -1,19 +1,19 @@
 import time
 import locale
-import datetime
+from datetime import datetime
 
 
 # Set locale to use comma as a thousands separator
-locale.setlocale(locale.LC_ALL, '')
+#locale.setlocale(locale.LC_ALL, '')
 
 # Current date and time -> floating-point number
 current_time = time.time()
-formatted_current_time = locale.format_string("%.4f", current_time, grouping=True)
+#formatted_current_time = locale.format_string("%.4f", current_time, grouping=True)
 
 # Specific date
-current_date = datetime.date.today()
+current_date = datetime.fromtimestamp(current_time)
 
-print(f"Seconds since January 1, 1970: {formatted_current_time} or {current_time:.2e} in scientific notation")
+print(f"Seconds since January 1, 1970: {current_time:,.4f} or {current_time:.2e} in scientific notation")
 #print(f"{current_date}")
 #print(f"{current_date.strftime('%B')} {current_date.day} {current_date.year}")
-print(f"{current_date.strftime('%b %d %Y')}")
+print(current_date.strftime("%b %d %Y"))

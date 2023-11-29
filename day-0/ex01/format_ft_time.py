1424e51012eefffca1ee484dcf95d4d5ca8355cf
Stephane Walter <swalter@k2r3p7.42mulhouse.fr>
2023-11-29 18:34:32 +0100

    day4

--- day-0/ex01/format_ft_time.py
+++ day-0/ex01/format_ft_time.py
@@ -7,6 +7,5 @@ current_time = time.time()
 # Specific date
 current_date = datetime.fromtimestamp(current_time)
 
-print(f"Seconds since January 1, 1970: {current_time:,.4f}\
-      or {current_time:.2e} in scientific notation")
+print(f"Seconds since January 1, 1970: {current_time:,.4f} or {current_time:.2e} in scientific notation")
 print(current_date.strftime("%b %d %Y"))
